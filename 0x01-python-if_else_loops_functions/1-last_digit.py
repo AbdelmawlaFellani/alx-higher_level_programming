5e2822fcc100993c673417276c391b9f515a6574
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-10-03 14:27:36 +0100

    Deleted unnecessary files

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-nb_abs = number if number >= 0 else number * -1
+nb_abs = number if number >= 0 else abs(number)
 last_digit = nb_abs % 10
-greater_than_5 = "greater" if last_digit > 5 else "less"
+greater = "greater" if last_digit > 5 else "less"
 not_zero = "not" if last_digit != 0 else "is"
-print(f'Last digit of {number} is {last_digit} and is {greater_than_5}\
-than 5 and {not_zero} 0')
+print(f'Last digit of {number} is {last_digit} and is {greater}\
+ than 5 and {not_zero} 0')
