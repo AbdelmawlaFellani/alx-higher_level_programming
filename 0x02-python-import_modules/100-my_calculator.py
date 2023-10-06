a8b973a6d5591ff8b9fed71b46f4496e3c715e5d
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-10-06 12:18:26 +0100

    Advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 from calculator_1 import add, mul, sub, div
 if __name__ = "__main__":
-    import sys
+    from sys import argv, exit
 
-    count = len(sys.argv) - 1
-    if count != 3:
+    if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    op = sys.argv[2]
-    if op == "+":
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op == "-":
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op == "*": 
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op == "/":       
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
-    else:
+        exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    if op not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1) 
+        exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, ops[op](a, b)))
