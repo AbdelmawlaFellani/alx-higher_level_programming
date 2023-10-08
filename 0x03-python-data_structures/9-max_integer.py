3052ad669d6dae48e4b7c261fd41141df07a927f
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-10-08 14:36:34 +0100

    9. Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max_val = my_list[0]
+        for elm in my_list:
+            if elm >= max_val:
+                max_val = elm
+    return max_val
