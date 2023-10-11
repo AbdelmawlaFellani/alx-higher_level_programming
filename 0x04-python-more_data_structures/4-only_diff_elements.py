cf13a7bddf40f565611ac918fd1892311768c268
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-10-11 21:52:06 +0100

    Task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,4 +2,4 @@
 def only_diff_elements(set_1, set_2):
     s1 = set(filter(lambda x: x not in set_1, set_2))
     s2 = set(filter(lambda x: x not in set_2, set_1))
-    return s1 | s2 
+    return s1 | s2
