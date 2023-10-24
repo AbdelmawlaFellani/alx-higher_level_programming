fc9ff1fcf5a3f819f07cbc8fd33deb7a47a5fba9
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-10-24 15:29:43 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class """
+
+
+class Square:
+    """A class to represent a Square shape"""
+
+    def __init__(self, size):
+        """Initialize the size of the Square"""
+        self.__size = size
