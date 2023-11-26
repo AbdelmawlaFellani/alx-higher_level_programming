9d76e860dfc2454e24f538d8e761d2d496970741
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-11-26 16:34:51 +0100

    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module for from_json_string function """
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+        Method for from_json_string
+
+        Args: 
+            my_str (str): The string provided
+
+        Return:
+            object (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
