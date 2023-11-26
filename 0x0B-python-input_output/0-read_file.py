7242266901714a7e43485f514f318f897ac164bb
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-11-26 15:51:22 +0100

    Read File

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" this Module is about reading a file """
+
 
 def read_file(filename=""):
+    """
+    Method for read_file.
+
+    Args:
+        filename (str): The file name.
+    """
     with open(filename, encoding="utf-8") as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
