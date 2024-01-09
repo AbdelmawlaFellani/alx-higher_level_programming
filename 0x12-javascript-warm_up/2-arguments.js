6846eb127aa71ea957f144c80964baeca941d8ba
AbdelmawlaFellani <fellaniabdo@gmail.com>
2024-01-09 01:18:22 +0100

    Arguments ...

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (argv.length > 2) { console.log('Argument found'); } else { console.log('No argument'); }
+if (argv.length > 1) { console.log('Argument found'); } else { console.log('No argument'); }
