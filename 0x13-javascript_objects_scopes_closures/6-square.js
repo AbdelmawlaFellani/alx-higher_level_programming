dbfeecbb7e6c291506b020b5139353e0d7d25485
AbdelmawlaFellani <fellaniabdo@gmail.com>
2024-01-10 00:53:22 +0100

    Square Task 6 JS

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      if (c === undefined) { console.log('X'.repeat(this.size)); } else { console.log(c.repeat(this.size)); }
+    }
+  }
+};
