b62d47fa033d69445118f679be3f871a007f316d
AbdelmawlaFellani <fellaniabdo@gmail.com>
2024-01-10 02:12:57 +0100

    Advanced JS

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newList = [];
-list.map((e, index) => {
-  newList.push(e * index);
-});
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
